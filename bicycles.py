3dd02702e46fb5647e312e17bdb89ce66c4ee635
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2020-11-15 08:23:25 -0600

    motorcycles

--- bicycles.py
+++ bicycles.py
@@ -1,2 +1,22 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles) 
+
+print(bicycles[0])
+
+print(bicycles[0].title()) 
+
+print(bicycles[1])
+print(bicycles[3]) 
+
+print(bicycles[-1])
+
+print(bicycles[-1].lower())
+
+print(bicycles[3].lower())
+
+print(bicycles[-1] == bicycles[3]) 
+
+print(bicycles[-4].lower() == bicycles[0].lower()) 
+
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message) 
---
+++ motorcycles.py
@@ -0,0 +1,22 @@
+#motorcycles.py
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+# motorcycles[0] = 'ducati'
+# print(motorcycles)
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+# motorcycles.append('ducati')
+# print(motorcycles) 
+
+# motorcycles = []
+# print(motorcycles) 
+# motorcycles.append('honda')
+# motorcycles.append('yamaha')
+# motorcycles.append('suzuki')
+# print(motorcycles) 
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles) 
+motorcycles.insert(0, 'ducati')
+print(motorcycles) 
