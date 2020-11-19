f5f232fca4986e191a515ef68f6f1d57e0e3d245
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2020-11-19 12:13:38 -0600

    Chapter 5

---
+++ chapter05/alien_colors_2.py
@@ -0,0 +1,9 @@
+#alien_colors_2.py 
+
+#alien_color = 'green'
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
--- chapter05/toppings.py
+++ chapter05/toppings.py
@@ -1,4 +1,19 @@
 # toppings.py
-requested_topping = 'mushrooms'
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!") 
+
+# requested_topping = 'mushrooms'
+# if requested_topping != 'anchovies':
+#     print("Hold the anchovies!") 
+
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+# elif 'pepperoni' in requested_toppings:
+#     print("Adding pepperoni.")
+# elif 'extra cheese' in requested_toppings:
+#     print("Adding extra cheese.")    
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+    
+print("\nFinished making your pizza!")
