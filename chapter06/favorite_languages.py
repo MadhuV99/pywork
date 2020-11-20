cf693ee0c871710617dbf1c835873b0cafc1a35c
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2020-11-19 19:34:37 -0600

    Chapter 6

--- chapter06/favorite_languages.py
+++ chapter06/favorite_languages.py
@@ -6,6 +6,47 @@ favorite_languages = {
 'edward': 'ruby',
 'phil': 'python',
 } 
-print(favorite_languages)
-language = favorite_languages['sarah'].title()
-print(f"Sarah's favorite language is {language}.")
+# print(favorite_languages)
+# language = favorite_languages['sarah'].title()
+# print(f"Sarah's favorite language is {language}.")
+
+# for name, language in favorite_languages.items():
+#     print(f"{name.title()}'s favorite language is {language.title()}.")
+
+# for name in favorite_languages.keys():
+#     print(name.title())    
+
+# for name in favorite_languages:
+#     print(name.title())        
+
+# friends = ['phil', 'sarah']
+# # for name in favorite_languages.keys():
+# for name in favorite_languages:
+#     print(name.title())
+#     if name in friends:
+#         language = favorite_languages[name].title()
+#         print(f"\t{name.title()}, I see you love {language}!")
+
+# print(favorite_languages)
+# print(favorite_languages.keys())
+# keys = favorite_languages.keys()
+# print(type(keys))
+# if 'erin' not in favorite_languages.keys():
+#     print("Erin, please take our poll!")
+
+# for name in sorted(favorite_languages.keys()):
+#     print(f"{name.title()}, thank you for taking the poll.")
+
+# print(favorite_languages.values())
+# values = favorite_languages.values()
+# print(type(values))
+# print("The following languages have been mentioned:")
+# for language in favorite_languages.values():
+#     print(language.title())
+
+setofvalues = set(favorite_languages.values())
+print(setofvalues)
+print(type(setofvalues))
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
