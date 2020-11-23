a5f13f25a8e165568a246d35568830b0e5474648
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2020-11-22 20:22:58 -0600

    Chapter 8

--- chapter08/greeter.py
+++ chapter08/greeter.py
@@ -1,7 +1,13 @@
 # greeter.py
-def greet_user():
-"""Display a simple greeting."""
-print("Hello!")
 
-greet_user()
+# def greet_user():
+#     """Display a simple greeting."""
+#     print("Hello!")
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+
+# greet_user()
+greet_user('jesse')
+greet_user('sarah')
 
---
+++ chapter08/pets.py
@@ -0,0 +1,32 @@
+# pets.py
+
+# def describe_pet(animal_type, pet_name):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# # describe_pet('hamster', 'harry')
+# # describe_pet('harry', 'hamster')
+# # describe_pet('dog', 'willie') 
+# # describe_pet(animal_type='dog', pet_name='willie') 
+# describe_pet(pet_name='willie', animal_type='dog') 
+
+def describe_pet(pet_name, animal_type='dog'):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# describe_pet(pet_name='willie')
+# describe_pet('willie') 
+# describe_pet(pet_name='harry', animal_type='hamster')
+# describe_pet(animal_type='hamster', pet_name='harry') 
+
+# A dog named Willie.
+describe_pet('willie')
+describe_pet(pet_name='willie')
+# A hamster named Harry.
+describe_pet('harry', 'hamster')
+describe_pet(pet_name='harry', animal_type='hamster')
+describe_pet(animal_type='hamster', pet_name='harry') 
+
+
