4d68790d9c793f63ccc3425a671dc0abd960b84c
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2020-11-24 14:43:03 -0600

    Chapter 9

--- chapter09/dog.py
+++ chapter09/dog.py
@@ -19,3 +19,11 @@ class Dog:
 my_dog = Dog('Willie', 6)
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age} years old.") 
+my_dog.sit()
+my_dog.roll_over()
+
+your_dog = Dog('Lucy', 3)
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit() 
+
