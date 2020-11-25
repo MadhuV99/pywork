1b3bd58a3a382ff28297467ef02338a0db2bc0ad
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2020-11-24 20:22:09 -0600

    Chapter 9

--- chapter09/electric_car.py
+++ chapter09/electric_car.py
@@ -24,6 +24,29 @@ class Car:
     def increment_odometer(self, miles):
         self.odometer_reading += miles
 
+    def fill_gas_tank(self):
+        """Cars need gas tank refills."""
+        print(f"{self.year} {self.make} {self.model} is re-fuelling...")
+
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.") 
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        print(f"This car can go about {range} miles on a full charge.")        
+
+
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
     def __init__(self, make, model, year):
@@ -32,12 +55,29 @@ class ElectricCar(Car):
         Then initialize attributes specific to an electric car.
         """
         super().__init__(make, model, year)
-        self.battery_size = 75
+        # self.battery_size = 75
+        self.battery = Battery()        
         
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+    # def describe_battery(self):
+    #     """Print a statement describing the battery size."""
+    #     print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def fill_gas_tank(self):
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't need a gas tank!") 
+
+my_used_car = Car('subaru', 'outback', 2015)
+print(my_used_car.get_descriptive_name())
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer() 
+my_used_car.fill_gas_tank()
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name()) 
-my_tesla.describe_battery()
+my_tesla.fill_gas_tank()
+# my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
+print(f"Battery-size: {my_tesla.battery.battery_size} KWh")
+my_tesla.battery.get_range()
